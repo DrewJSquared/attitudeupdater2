5ca98fc5bc2c373593127ba5b0b58349bf73c36c
DrewJSquared <drew@jsquaredsystems.com>
2024-01-13 15:54:00 -0600

    add web request for update

--- autoupdate2.js
+++ autoupdate2.js
@@ -8,6 +8,7 @@ var log = require('npmlog');
 var cron = require('node-cron');
 var fs = require('fs');
 const download = require('download-git-repo');
+const https = require("https");
 
 
 // cron update every night at 3am: 0 3 * * *
@@ -17,6 +18,9 @@ cron.schedule('0 3 * * *', () => {
 
 
 // variables
+var DEVICE_ID = 0;
+var SERIALNUMBER = 'AC-00200XX';
+
 var updaterCounter = 0;
 var downloadSuccess = false;
 var downloadTimedOut = false;
@@ -32,28 +36,39 @@ var DOWNLOAD_TIMEOUT_LENGTH = 60000;
 // primary update function
 function updater() {
 	log.info('AUTOUPDATE', 'Automatic Update Script for Attitude Control Device Firmware');
-	log.info('AUTOUPDATE', ' +++ 2nd generation +++ (fixed timing 011324) ');
-	log.info('AUTOUPDATE', 'Copyright 2024 Drew Shipps, J Squared Systems');
+	log.info('AUTOUPDATE', ' +++ 2nd generation +++ (fix timing & add web req 011324)');
+	log.info('AUTOUPDATE', 'Copyright 2023 Drew Shipps, J Squared Systems');
 
-	updaterCounter++;
-	if (updaterCounter > 5) {
-		log.error('AUTOUPDATE', 'This is sad. Autoupdaer attempted to run 5 times, but has somehow failed every single time! Unfortunately, this means this device will become offline until the next autoupdate cycle. Sorry!');
-		restart();
-	}
+	// load device ID from id.json
+	loadDeviceID(function () {
+		// (callback for once ID loaded)
 
-	// attempt to download new code
-	downloadNewCode(function () {
-		if (!downloadTimedOut) {
-			downloadSuccess = true;
+		// check attitude server for update via HTTPS (use device ID to see if an update is needed)
+		checkAttitudeServerForUpdate(function () {
+			// (callback in case an update is required)
 
-			downloadFinished();
-		}
+			// now we finally attempt to update
+			updaterCounter++;
+			if (updaterCounter > 5) {
+				log.error('AUTOUPDATE', 'This is sad. Autoupdaer attempted to run 5 times, but has somehow failed every single time! Unfortunately, this means this device will become offline until the next autoupdate cycle. Sorry!');
+				restart();
+			}
+
+			// attempt to download new code
+			downloadNewCode(function () {
+				if (!downloadTimedOut) {
+					downloadSuccess = true;
+
+					downloadFinished();
+				}
+			});
+
+			// after timeout interval passes, run finished function anyway
+			setTimeout(function () {
+				downloadFinished();
+			}, DOWNLOAD_TIMEOUT_LENGTH);
+		});
 	});
-
-	// after timeout interval passes, run finished function anyway
-	setTimeout(function () {
-		downloadFinished();
-	}, DOWNLOAD_TIMEOUT_LENGTH);
 }
 
 
@@ -262,6 +277,97 @@ function revertBackupToCurrent(callback) {
 			log.info('AUTOUPDATE', 'No backup code found! Attempting to run updater again...');
 			updater();
 		}
+	});
+}
+
+
+// loadDeviceID - load Device ID from id.json
+function loadDeviceID(callback) {
+	var path = './id.json';
+
+	try {
+	  	let rawdata = fs.readFileSync(path);
+	
+	  	try {
+		  	let data = JSON.parse(rawdata);
+
+			DEVICE_ID = data.device_id;
+			SERIALNUMBER = data.serialnumber;
+
+			// if either does not update properly then crash the app
+			if (!Number.isInteger(DEVICE_ID) || typeof SERIALNUMBER != 'string') {
+				log.error('AUTOUPDATE', 'Failed to initialize Device ID and/or Serial Number.');
+				// process.exit();
+			}
+
+		  	log.info('AUTOUPDATE', 'Device ID: ' + DEVICE_ID + ', Serial Number: ' + SERIALNUMBER);
+
+		  	callback();
+		}
+		catch(err) {
+		  	log.error('AUTOUPDATE', 'JSON.parse(rawdata) error or some other error inside callback...');
+		  	log.error('AUTOUPDATE', 'Error: ' + err.message);
+			// process.exit();
+		}
+	}
+	catch(err) {
+	  	log.error('AUTOUPDATE', 'id.json file not found! Failed to load device ID!');
+	  	log.error('AUTOUPDATE', 'Error: ' + err.message);
+		// process.exit();
+	}
+}
+
+
+// checkAttitudeServerForUpdate - check attitude server to see if an update is needed, if so call callback else exit
+function checkAttitudeServerForUpdate(callback) {
+	// setup url
+	var url = 'https://attitude.lighting/api/devices/' + DEVICE_ID + '/autoupdate';
+
+	log.http('AUTOUPDATE', 'Attempting to get the autoupdate flag from the attitude.lighting server.');
+	log.http('AUTOUPDATE', 'URL: ' + url);
+
+	// actual https get
+	https.get(url, resp => {
+		let data = "";
+
+		// process each chunk
+		resp.on("data", chunk => {
+			data += chunk;
+		});
+
+		// finished, do something with result
+		resp.on("end", () => {
+			if (resp.statusCode == 200) {
+				// 200 ok response so continue
+
+				// check data
+				if (data == '1') {
+					// update!
+					log.http('AUTOUPDATE', '::: Update requested by attitude.lighting server! :::');
+					log.info('AUTOUPDATE', 'Running automatic update script callback...');
+
+					callback();
+				} else if (data == '0') {
+					// no need for update
+					log.info('AUTOUPDATE', 'No update needed :)');
+					log.info('AUTOUPDATE', 'Successfully completed autoupdate script!');
+				} else {
+					// unknown response
+					log.error('HTTPS', 'Unknown response from server:');
+					log.error('HTTPS', 'Data: ' + data);
+					log.error('AUTOUPDATE', 'Autoupdate FAILED!');
+					console.log('Autoupdate failed due to unknown data response');
+				}
+			} else {
+				// anything else is bad news and means something is wrong with the server or request
+				log.error('HTTPS', 'Error status code is ' + resp.statusCode);
+				log.error('AUTOUPDATE', 'Autoupdate FAILED!');
+				console.log('Autoupdate failed due to HTTPS status code ' + resp.statusCode);
+			}
+		});
+	}).on("error", err => {
+		log.error('HTTPS', 'Error: ' + err.message);
+		log.error('AUTOUPDATE', 'Unable to get autoupdate status from attitude.lighting server! AUTOUPDATER FAILED!!');
 	});
 }
 
